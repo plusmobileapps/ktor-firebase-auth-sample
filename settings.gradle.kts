2a9af4d9d02063d331a4cac275e3a0bd7a703c12
Andrew Steinmetz <andrew@plusmobileapps.com>
2022-08-01 09:51:42 -0700

    initial commit of ktor started project - auth, content negotiation, kotlinx serialization, routing

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.plusmobileapps.ktor-firebase-admin-sample"
---
+++ src/main/kotlin/com/plusmobileapps/firebaseadminsample/plugins/Security.kt
@@ -0,0 +1,12 @@
+package com.plusmobileapps.firebaseadminsample.plugins
+
+import io.ktor.server.auth.*
+import io.ktor.util.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+
+fun Application.configureFirebaseAuth() {
+    
+
+}
